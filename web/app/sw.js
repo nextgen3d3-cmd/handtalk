aa2ca83258a5e155d93a078d6e5409bba9b21a15
Jean Gatica <jeangaticag@gmail.com>
2026-09-24 11:05:21 -0400

    Credito Nexo Systems

--- web/app/sw.js
+++ web/app/sw.js
@@ -1,7 +1,7 @@
 // Service worker de HandTalk: guarda la app para que abra aunque no haya internet.
-const CACHE = 'handtalk-v2';
+const CACHE = 'handtalk-v3';
 const FILES = ['./', 'index.html', 'manifest.webmanifest', 'icons/icon-192.png', 'icons/icon-512.png',
-               'img/1.jpeg', 'img/2.jpeg', 'img/3.jpeg'];
+               'img/1.jpeg', 'img/2.jpeg', 'img/3.jpeg', 'img/nexo-systems.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
